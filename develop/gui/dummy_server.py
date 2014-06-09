ea45f8d067c63d7d5f16fdce1ca6f463f9417d34
Craig Haywood <c.haywood@smartdigitaloptics.com>
2014-06-09 15:27:19 +1000

    completed implementation of model, successfully reading/writing JSON config from dummy server

--- develop/gui/dummy_server.py
+++ develop/gui/dummy_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 # Echo server program
+import json
 import socket
 import socketio
 
@@ -8,27 +9,41 @@ CONFIG_FILE=".sib_config.JSON"
 
 # ------ COMMANDS ------ #
 GET_CONFIG 	= "get_conf"
+SET_CONFIG	= "set_conf"
 
 HOST = 'localhost'                  # Symbolic name meaning all available interfaces
 PORT = 46781              			# Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
-conn, addr = s.accept()
-print('Connected by', addr)
-
-settings_file = open(CONFIG_FILE, 'r')
-contents = settings_file.read()
-contents_length = len(contents)
-print("Length: {0}".format(contents_length))
 
 while True:
-	data = socketio.receive(conn)
-	if not data:
-		break
-	print("received:{0}".format(data))
-	if data == GET_CONFIG:
-		print("received send all config command")
-		socketio.send(conn, contents)
+	conn, addr = s.accept()
+	print('Connected by', addr)
 
-conn.close()
+	settings_file = open(CONFIG_FILE, 'r')
+	contents = settings_file.read()
+	settings_file.close()
+
+	config = json.loads(contents)
+
+	while True:
+		data = socketio.receive(conn)
+		if not data:
+			break
+		print("received:{0}".format(data))
+		if data == GET_CONFIG:
+			print("received send all config command")
+			socketio.send(conn, contents)
+		if data == SET_CONFIG:
+			print("received set config command")
+			page = socketio.receive(conn)
+			key = socketio.receive(conn)
+			value = socketio.receive(conn)
+			print("Received page:{0} key:{1} value:{2}".format(page, key, value))
+			config['SETTINGS'][page][key] = json.loads(value)
+			settings_file = open(CONFIG_FILE, 'w')
+			settings_file.write(json.dumps(config, sort_keys=True, indent=4, separators=(',', ': ')))
+			settings_file.close()
+
+	conn.close()
--- develop/gui/model.py
+++ develop/gui/model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import threading
 import fcntl
 import logging
 import socket
@@ -16,6 +17,7 @@ SETTINGS_FILE 	= '.sib_settings.JSON'
 
 # ------ COMMANDS ------ #
 GET_CONFIG 	= "get_conf"
+SET_CONFIG	= "set_conf"
 
 def check_singleton():
 	global lock_file
@@ -25,7 +27,7 @@ def check_singleton():
 
 		return True
 	except Exception as e:
-		logging.error("Failed aquire exclusive lock on file: " + UI_LK_FILE)
+		logging.error("Failed to aquire exclusive lock on file: " + UI_LK_FILE)
 
 		return False
 
@@ -48,13 +50,34 @@ def load_default_settings():
 							"port" 			: 46781	\
 						}')
 
+class updateThread(threading.Thread):
+	def __init__(self, domain, port, page, key, value):
+		threading.Thread.__init__(self)
+		self.domain = domain
+		self.port = port
+		self.page = page
+		self.key = key
+		self.value = value
+	def run(self):
+		if DEBUG:
+			t = threading.current_thread();
+			print("{0}: Updating sib server with page:{1} key:{2} value:{3}".format(t, self.page, self.key, self.value))
+		conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		conn.connect((self.domain, self.port))
+		socketio.send(conn, SET_CONFIG)
+		socketio.send(conn, self.page)
+		socketio.send(conn, self.key)
+		socketio.send(conn, json.dumps(self.value))
+		conn.close()
+
 class Model():
 	conn = -1
 	config = -1
 
 	def __init__(self, settings):
 		if DEBUG:
-			print("Model constructor init")
+			t = threading.current_thread();
+			print("{0}: Model constructor init".format(t))
 		logging.basicConfig(level=settings['logging_level'])
 
 		self.connect_to_sib('localhost', settings['port'])
@@ -63,27 +86,73 @@ class Model():
 
 	def connect_to_sib(self, domain, port):
 		if DEBUG:
-			print("Connecting to SIB, domain:{0}, port:{1}".format(domain, port))
+			t = threading.current_thread();
+			print("{0}: Connecting to SIB, domain:{1} port:{2}".format(t, domain, port))
 		self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.conn.connect((domain, port))
 
 	def disconnect_from_sib(self):
 		if DEBUG:
-			print("Diconnecting from SIB")
+			t = threading.current_thread();
+			print("{0}: Diconnecting from SIB".format(t))
 		self.conn.close()
 
 	def load_all_config(self):
 		if DEBUG:
-			print("Loading SIB configuration")
+			t = threading.current_thread();
+			print("{0}: Loading SIB configuration".format(t))
 		socketio.send(self.conn, GET_CONFIG)
 		config_str = socketio.receive(self.conn)
 		if DEBUG:
 			print("-------------------------------------------------------------------")
 			print(config_str)
 			print("-------------------------------------------------------------------")
-		config = json.loads(config_str)
-		
+		self.config = json.loads(config_str)
+	
+	def get_config(self, page, key):
+		try:
+			value = self.config['SETTINGS'][page][key]
+			if DEBUG:
+				t = threading.current_thread();
+				print("{0}: Config for page:{1} key:{2} is {3}".format(t, page, key, value))
+			
+			return value
+		except Exception as e:
+			logging.error("Failed to retrieve value for page:{1} key:{2}".format(t, page, key))
+			
+			return None
 
+	def set_config(self, page, key, value):
+		try:
+			tmp = self.config['SETTINGS'][page]
+		except Exception as e:
+			if DEBUG:
+				t = threading.current_thread();
+				print("{0}: Page {1} does not exist in config".format(t, page))
+			return
+		try:
+			tmp = self.config['SETTINGS'][page][key]
+		except Exception as e:
+			if DEBUG:
+				t = threading.current_thread();
+				print("{0}: Key {1} of page {2} does not exist in config".format(t, key, page))
+			return
+		
+		key_type = type(self.config['SETTINGS'][page][key])
+		value_type = type(value)
+		if value_type != key_type:
+			if DEBUG:
+				t = threading.current_thread();
+				print("{0}: Failed to set value={1} for page:{2} key:{3}: TypeError".format(t, value, page, key))
+			return
+		
+		self.config['SETTINGS'][page][key] = value
+		if DEBUG:
+			t = threading.current_thread();
+			print("{0}: Config for page:{1} key:{2} is {3}".format(t, page, key, value))
+		thread = updateThread('localhost', settings['port'], page, key, value)
+		thread.start()
+		
 if __name__ == '__main__':
 	isSingleton = check_singleton()
 	if isSingleton == False:
@@ -91,4 +160,6 @@ if __name__ == '__main__':
 		sys.exit(0)
 	settings = load_settings()
 	model = Model(settings)
-	
+	val = model.get_config('GENERAL', 'Launch_on_startup')
+	model.set_config('UNTRUSTED_PEERS', 'Cap_untrusted_peers', True)
+	model.set_config('UNTRUSTED_PEERS', 'Untrusted_peers_cap', 400)
